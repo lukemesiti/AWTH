be1ff967f92d6d602f70674fbcc0ac6f203d6159
Luke Mesiti <lukemesiti@gmail.com>
2023-07-30 10:19:02 +1000

    update Content component formatting

--- src/layout/content/Content.tsx
+++ src/layout/content/Content.tsx
@@ -4,8 +4,7 @@ const Content: React.FC = () => {
   return (
     <main className="flex-auto flex items-center justify-center p-3">
       <div className="text-center">
-        <h1>A better way</h1>
-        <h1>to enjoy every day.</h1>
+        <h1>A better way to enjoy every day.</h1>
         <p>Be the first to know when we launch.</p>
         <RequestInviteButton />
       </div>
