54a4275209da88c91cf35c0b844182251af01b40
Luke Mesiti <lukemesiti@gmail.com>
2023-08-04 08:48:31 +1000

    update invite button click test

--- src/layout/content/RequestInviteButton.test.tsx
+++ src/layout/content/RequestInviteButton.test.tsx
@@ -1,9 +1,13 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { describe, expect, it } from "vitest";
+import {
+  REQUEST_BUTTON_TEXT,
+  REQUEST_INVITE_BUTTON_TEST_ID,
+  REQUEST_INVITE_FORM_TEST_ID,
+} from ".";
 import { render } from "../../utils/testUtils";
 import { RequestInviteButton } from "./RequestInviteButton";
-import { REQUEST_INVITE_BUTTON_TEST_ID, REQUEST_INVITE_FORM_TEST_ID } from ".";
 
 global.ResizeObserver = class FakeResizeObserver {
   observe() {}
@@ -11,7 +15,7 @@ global.ResizeObserver = class FakeResizeObserver {
   unobserve() {}
 };
 
-describe("RequestInviteButton", () => {
+describe("RequestInviteButton", async () => {
   it("should render with text", () => {
     // Arrange
 
@@ -22,17 +26,20 @@ describe("RequestInviteButton", () => {
     expect(
       screen.getByTestId(REQUEST_INVITE_BUTTON_TEST_ID)
     ).toBeInTheDocument();
-    expect(screen.getByText("Request an invite")).toBeInTheDocument();
+    expect(screen.getByText(REQUEST_BUTTON_TEXT)).toBeInTheDocument();
   });
 
   it("should open the request invite modal when clicked", async () => {
     // Arrange
+    const user = userEvent.setup();
     render(<RequestInviteButton />);
 
     // Act
-    await userEvent.click(screen.getByText("Request an invite"));
+    await user.click(screen.getByText(REQUEST_BUTTON_TEXT));
 
     // Assert
-    expect(screen.getByTestId(REQUEST_INVITE_FORM_TEST_ID)).toBeInTheDocument();
+    await screen.findByTestId(REQUEST_INVITE_FORM_TEST_ID).then(() => {
+      expect(screen.getByTestId(REQUEST_INVITE_FORM_TEST_ID)).toBeVisible();
+    });
   });
 });
--- src/layout/content/RequestInviteButton.tsx
+++ src/layout/content/RequestInviteButton.tsx
@@ -2,14 +2,16 @@ import { BUIButton } from "../../components";
 import { RequestInviteForm } from "../../inviteForm";
 import { SuccessModal } from "../../successMessage";
 import { useModalDisplay } from "../../context";
-import { REQUEST_INVITE_BUTTON_TEST_ID } from ".";
+import { REQUEST_BUTTON_TEXT, REQUEST_INVITE_BUTTON_TEST_ID } from ".";
 
 const RequestInviteButton: React.FC = () => {
   const { setModal } = useModalDisplay();
 
   return (
     <div data-testid={REQUEST_INVITE_BUTTON_TEST_ID}>
-      <BUIButton onClick={() => setModal("form")}>Request an invite</BUIButton>
+      <BUIButton onClick={() => setModal("form")}>
+        {REQUEST_BUTTON_TEXT}
+      </BUIButton>
       <RequestInviteForm />
       <SuccessModal />
     </div>
