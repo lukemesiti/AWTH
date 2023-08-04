7e423ce89de7a6843ae090492a2dca985230d5d6
Luke Mesiti <lukemesiti@gmail.com>
2023-08-04 14:44:03 +1000

    update header styling

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,12 +2,8 @@ import { HEADER_TEST_ID } from "./types";
 
 const Header: React.FC = () => {
   return (
-    <header data-testid={HEADER_TEST_ID}>
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-        <div className="flex lg:flex-1">
-          <h1 className="text-3xl font-bold -m-1.5 p-1.5 ">🥦Broccoli & Co.</h1>
-        </div>
-      </nav>
+    <header className="p-8" data-testid={HEADER_TEST_ID}>
+      <h1 className="text-3xl font-bold">🥦Broccoli & Co.</h1>
     </header>
   );
 };
