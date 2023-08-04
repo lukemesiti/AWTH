c60080b9f4df9cd0dbc65fba6e22f9fc2924bc77
Luke Mesiti <lukemesiti@gmail.com>
2023-08-04 15:11:05 +1000

    adjust header and footer responsive styling

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,7 +2,10 @@ import { FOOTER_TEST_ID } from "./types";
 
 const Footer: React.FC = () => {
   return (
-    <footer data-testid={FOOTER_TEST_ID} className="text-center p-8 text-lg">
+    <footer
+      data-testid={FOOTER_TEST_ID}
+      className="text-center py-8 px-1 md:text-lg sm:text-md"
+    >
       <div>Made with 💚➕🍝 in Melbourne.</div>
       <div>©️ 2023 Broccoli & Co. All rights reserved.</div>
     </footer>
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,10 +2,18 @@ import { HEADER_TEST_ID } from "./types";
 
 const Header: React.FC = () => {
   return (
-    <header className="p-8" data-testid={HEADER_TEST_ID}>
-      <h1 className="text-3xl font-bold">🥦Broccoli & Co.</h1>
+    <header data-testid={HEADER_TEST_ID}>
+      <nav className="mx-auto max-w-7xl md:p-8 p-4">
+        <h1 className="text-3xl font-bold">🥦Broccoli & Co.</h1>
+      </nav>
     </header>
   );
 };
 
 export { Header };
+
+{
+  /* <header className="p-8" data-testid={HEADER_TEST_ID}>
+<h1 className="text-3xl font-bold">🥦Broccoli & Co.</h1>
+</header> */
+}
